dc5960a615fd41602c17a4fa4480e2d8425c407b
Me 198995 <198995@ezchat.nfshost.com>
2012-05-13 02:30:23 +0000

    One liner change

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,5 +1,5 @@
 <html>
 <head>
-    <title><?php if (isset($title)) echo $title ?></title>
     <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+    <title><?php if (isset($title)) echo $title ?></title>
 <!-- /head and body are provided within each individual page-->
